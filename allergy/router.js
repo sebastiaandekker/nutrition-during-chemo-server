61c40bf56410932687a050e39a65923d59414e5a
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-03-30 10:31:54 +0200

    feat(allergy): create allergy router with create allergy route

---
+++ allergy/router.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const Allergy = require("./model");
+// const User = require("./model");
+// const auth = require("../auth/middleware");
+// const { toJWT } = require("../auth/jwt");
+
+const router = new Router();
+
+// Create medical information
+router.post("/allergy", (req, res, next) => {
+  Allergy.create(req.body)
+    .then(allergy => {
+      res.status(201).send(allergy);
+    })
+    .catch(next);
+});
+
+module.exports = router;
