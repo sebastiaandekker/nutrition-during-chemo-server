248b519868564b6b7655f1e1dd4a751453d865c0
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-03-31 20:57:30 +0200

    feat(user-allergy): Adding timestamps false

--- user-allergy/model.js
+++ user-allergy/model.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
 
-const UserAllergy = db.define("user_allergy");
+const UserAllergy = db.define("user_allergy", {}, { timestamps: false });
 
 module.exports = UserAllergy;
