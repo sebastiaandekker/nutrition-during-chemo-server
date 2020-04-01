d44a890cfdb43e17768d483ec6540930b75350bf
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-04-01 09:12:27 +0200

    feat(user-allergy): Add primary key for super many-to-many relationship

--- user-allergy/model.js
+++ user-allergy/model.js
@@ -1,6 +1,17 @@
-const Sequelize = require("sequelize");
+const { DataTypes } = require("sequelize");
 const db = require("../db");
 
-const UserAllergy = db.define("user_allergy", {}, { timestamps: false });
+const UserAllergy = db.define(
+  "user_allergy",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    }
+  },
+  { timestamps: false }
+);
 
 module.exports = UserAllergy;
