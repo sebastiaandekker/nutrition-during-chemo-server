a80ebce05dc799dd7edee602ae35cc561daa0b13
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-04-03 12:11:10 +0200

    feat(tip): define tip model

---
+++ tip/model.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db");
+const User = require("../user/model");
+const FavoriteTip = require("../FavoriteTip/model");
+
+const Tip = db.define("Tip", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING
+  },
+  sources: {
+    type: DataTypes.ARRAY(DataTypes.STRING)
+  },
+  categories: {
+    type: DataTypes.ARRAY(DataTypes.STRING),
+    allowNull: false
+  }
+});
+
+User.belongsToMany(Tip, { through: FavoriteTip });
+Tip.belongsToMany(User, { through: FavoriteTip });
+User.hasMany(FavoriteTip);
+Tip.hasMany(FavoriteTip);
+FavoriteTip.belongsTo(User);
+FavoriteTip.belongsTo(Tip);
+
+module.exports = Tip;
